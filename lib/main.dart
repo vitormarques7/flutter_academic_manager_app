d3ace47d9c758019fbd01fedcae242421a8aba2e
Vitor <joaovitormarquesbr@gmail.com>
2026-05-27 10:22:08 -0300

    chore: integra Firebase ao aplicativo com configuração de autenticação e serviços (setup)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:academic_manager_app/config/routes/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'config/scroll/app_scroll_behavior.dart';
 import 'config/theme/app_theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
